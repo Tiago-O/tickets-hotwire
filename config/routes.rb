00fd5e9127e1a8909471c5e70f6460042a262415
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-10-19 21:59:30 +0100

    rails app to implement hotwire'

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  resources :tickets do
+    resources :comments, only: [:new, :create]
+  end
+  root to: 'tickets#index'
+end
